370382994560bc462d98cc3c7424318dbe940342
Keshav B <keshavbalaji2397@gmail.com>
2025-04-12 11:45:35 -0500

    e2 branch
    Co-authored-by: Kunal Jain <jainkunal2005@gmail.com>

---
+++ sim/src/POLARIS_GEM_e2_Simulator/vehicle_drivers/hybrid_a_star_sim/scripts/hybrid_astar_rs_node.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+import rospy
+import math
+import numpy as np
+from sensor_msgs.msg import NavSatFix, Imu #e4
+# Fix is for e2
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker, MarkerArray
+from tf.transformations import euler_from_quaternion
+import pyproj
+import argparse
+
+import sys
+import os
+
+# Ensure the `scripts` directory is at the very beginning of the Python path
+scripts_dir = os.path.dirname(__file__)
+if scripts_dir not in sys.path:
+    sys.path.insert(0, scripts_dir)
+
+print("printing paths in main(): ")
+print(sys.path)
+
+from astar_utils import hybrid_astar, plot_path
+from constants import STARTX, STARTY, STARTYAW
+
+
+current_utm = None
+current_yaw = None
+
+def gps_callback(msg):
+    global current_utm
+    lon, lat = msg.longitude, msg.latitude
+    utm_proj = pyproj.Proj(proj='utm', zone=16, ellps='WGS84')
+    x, y = utm_proj(lon, lat)
+    current_utm = (x, y)
+
+def imu_callback(msg):
+    global current_yaw
+    q = msg.orientation
+    _, _, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+    current_yaw = (yaw + np.pi) % (2*np.pi) # offset by 180, it's backwards for some reason
+    # print(current_yaw)
+
+def publish_path(path_points, offset_x, offset_y):
+    pub = rospy.Publisher('/waypoints', Path, queue_size=1, latch=True)
+    rospy.sleep(1.0)
+
+    path_msg = Path()
+    path_msg.header.frame_id = "map"
+    path_msg.header.stamp = rospy.Time.now()
+
+    for x, y, yaw in path_points:
+        pose = PoseStamped()
+        pose.header.frame_id = "map"
+        pose.pose.position.x = x - offset_x
+        pose.pose.position.y = y - offset_y
+        pose.pose.position.z = 0.0
+        pose.pose.orientation.x = 0.0
+        pose.pose.orientation.y = 0.0
+        pose.pose.orientation.z = math.sin(yaw / 2.0)
+        pose.pose.orientation.w = math.cos(yaw / 2.0)
+        path_msg.poses.append(pose)
+
+    pub.publish(path_msg)
+    rospy.loginfo(f"✅ Published {len(path_msg.poses)} waypoints to /waypoints (Gazebo frame)")
+
+def publish_path_markers(path_points, offset_x, offset_y):
+    marker_pub = rospy.Publisher('/path_markers', MarkerArray, queue_size=1, latch=True)
+    rospy.sleep(1.0)
+
+    marker_array = MarkerArray()
+    for i, (x, y, yaw) in enumerate(path_points):
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = "path_markers"
+        marker.id = i
+        marker.type = Marker.ARROW
+        marker.action = Marker.ADD
+        marker.pose.position.x = x - offset_x
+        marker.pose.position.y = y - offset_y
+        marker.pose.position.z = 0.0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = math.sin(yaw / 2.0)
+        marker.pose.orientation.w = math.cos(yaw / 2.0)
+        marker.scale.x = 0.5  # Arrow length
+        marker.scale.y = 0.1  # Arrow width
+        marker.scale.z = 0.1  # Arrow height
+        marker.color.a = 1.0  # Alpha
+        marker.color.r = 1.0  # Red
+        marker.color.g = 0.0  # Green
+        marker.color.b = 0.0  # Blue
+
+        marker_array.markers.append(marker)
+
+    marker_pub.publish(marker_array)
+    rospy.loginfo(f"✅ Published {len(marker_array.markers)} markers to /path_markers")
+
+def wait_for_pose():
+    global current_utm, current_yaw
+    while not rospy.is_shutdown() and (current_utm is None or current_yaw is None):
+        rospy.sleep(0.1)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--goal_x", type=float, required=True, help="Goal X in Gazebo map frame")
+    parser.add_argument("--goal_y", type=float, required=True, help="Goal Y in Gazebo map frame")
+    parser.add_argument("--goal_yaw", type=float, required=True, help="Goal yaw in degrees")
+    args = parser.parse_args()
+
+    rospy.init_node("hybrid_astar_rs_node")
+
+    rospy.Subscriber("/septentrio_gnss/navsatfix", NavSatFix, gps_callback)
+    rospy.Subscriber("/septentrio_gnss/imu", Imu, imu_callback)
+
+    rospy.loginfo("⌛ Waiting for GPS and IMU...")
+    wait_for_pose()
+    rospy.loginfo("✅ Received live GPS and IMU.")
+
+    # Get UTM start pose from live GPS
+    start_x, start_y = current_utm
+    start_yaw = current_yaw # we take this from sensor not ground truth, so 180
+    # start_yaw = math.radians(180) # fix it to face 180 degrees for now
+    start_pose = (start_x, start_y, start_yaw)
+
+    # GEM starts at Gazebo: x = -1.5, y = -21
+    gazebo_start_x = STARTX
+    gazebo_start_y = STARTY
+
+    # Offset between GPS UTM and Gazebo's map frame
+    offset_x = start_x - gazebo_start_x
+    offset_y = start_y - gazebo_start_y
+    print(f"start: {start_x, start_y}")
+    print(f"gazebo: {gazebo_start_x, gazebo_start_y}")
+    print(f"offsets: {offset_x, offset_y}")
+
+    # offset_x = 0
+    # offset_y = 0
+
+    # Convert local Gazebo goal to UTM
+    goal_x = args.goal_x + offset_x
+    goal_y = args.goal_y + offset_y
+    goal_yaw = math.radians(args.goal_yaw)
+    goal_pose = (goal_x, goal_y, goal_yaw)
+
+    rospy.loginfo("🚀 Planning path from live GPS to local goal...")
+    path = hybrid_astar(start_pose, goal_pose)
+
+    if path:
+        publish_path(path, offset_x, offset_y)
+
+        publish_path_markers(path, offset_x, offset_y) # off for now
+
+        # Optional debug plot in local frame
+        local_path = [(x - offset_x, y - offset_y, yaw) for x, y, yaw in path]
+        plot_path(local_path, (start_x - offset_x, start_y - offset_y, start_yaw),
+                  (goal_x - offset_x, goal_y - offset_y, goal_yaw))
+    else:
+        rospy.logerr("❌ Path planning failed.")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
---
+++ sim/src/POLARIS_GEM_e2_Simulator/vehicle_drivers/hybrid_a_star_sim/scripts/pure_pursuit_sim.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+import rospy
+import math
+import numpy as np
+from numpy import linalg as la
+
+from ackermann_msgs.msg import AckermannDrive
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
+from tf.transformations import euler_from_quaternion
+
+from gazebo_msgs.srv import GetModelState
+
+# GEM Sensor Headers
+from std_msgs.msg import String, Bool, Float32, Float64
+from sensor_msgs.msg import NavSatFix
+
+# GEM PACMod Headers
+from pacmod_msgs.msg import PositionWithSpeed, PacmodCmd, SystemRptFloat, VehicleSpeedRpt
+
+class PurePursuit(object):
+    
+    def __init__(self):
+        self.rate = rospy.Rate(20)
+        self.look_ahead = 6    # meters
+        self.wheelbase  = 1.75 # meters
+        self.goal = 0
+
+        # Waypoint storage
+        self.path_points_x = []
+        self.path_points_y = []
+        self.path_points_yaw = []
+        self.dist_arr = np.zeros(1)
+
+        # ROS Subscribers and Publishers
+        rospy.Subscriber("/waypoints", Path, self.path_callback)
+        self.ackermann_pub = rospy.Publisher('/ackermann_cmd', AckermannDrive, queue_size=1)
+
+        # -------------------- PACMod setup --------------------
+
+        self.gem_enable    = False
+        self.pacmod_enable = False
+
+        # GEM vehicle enable, publish once
+        self.enable_pub = rospy.Publisher('/pacmod/as_rx/enable', Bool, queue_size=1)
+        self.enable_cmd = Bool()
+        self.enable_cmd.data = False
+
+        # GEM vehicle gear control, neutral, forward and reverse, publish once
+        self.gear_pub = rospy.Publisher('/pacmod/as_rx/shift_cmd', PacmodCmd, queue_size=1)
+        self.gear_cmd = PacmodCmd()
+        self.gear_cmd.ui16_cmd = 2 # SHIFT_NEUTRAL
+
+        # GEM vehilce brake control
+        self.brake_pub = rospy.Publisher('/pacmod/as_rx/brake_cmd', PacmodCmd, queue_size=1)
+        self.brake_cmd = PacmodCmd()
+        self.brake_cmd.enable = False
+        self.brake_cmd.clear  = True
+        self.brake_cmd.ignore = True
+
+        # # Gem speed set to constant?
+        # self.speed_pub = rospy.Publisher('/pacmod/as_tx/vehicle_speed', PacmodCmd, queue_size=1)
+        # self.speed_cmd = PacmodCmd()
+
+        # GEM vechile forward motion control
+        self.accel_pub = rospy.Publisher('/pacmod/as_rx/accel_cmd', PacmodCmd, queue_size=1)
+        self.accel_cmd = PacmodCmd()
+        self.accel_cmd.enable = False
+        self.accel_cmd.clear  = True
+        self.accel_cmd.ignore = True
+
+        # GEM vechile turn signal control
+        self.turn_pub = rospy.Publisher('/pacmod/as_rx/turn_cmd', PacmodCmd, queue_size=1)
+        self.turn_cmd = PacmodCmd()
+        self.turn_cmd.ui16_cmd = 1 # None
+
+        # GEM vechile steering wheel control
+        self.steer_pub = rospy.Publisher('/pacmod/as_rx/steer_cmd', PositionWithSpeed, queue_size=1)
+        self.steer_cmd = PositionWithSpeed()
+        self.steer_cmd.angular_position = 0.0 # radians, -: clockwise, +: counter-clockwise
+        self.steer_cmd.angular_velocity_limit = 2.0 # radians/second
+
+    def path_callback(self, msg):
+        self.path_points_x = []
+        self.path_points_y = []
+        self.path_points_yaw = []
+
+        for pose in msg.poses:
+            x = pose.pose.position.x
+            y = pose.pose.position.y
+            quat = pose.pose.orientation
+            _, _, yaw = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
+
+            self.path_points_x.append(x)
+            self.path_points_y.append(y)
+            self.path_points_yaw.append(yaw)
+
+        self.dist_arr = np.zeros(len(self.path_points_x))
+        rospy.loginfo(f"✅ Received {len(self.path_points_x)} waypoints.")
+
+    def dist(self, p1, p2):
+        return round(np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2), 3)
+
+    def find_angle(self, v1, v2):
+        cosang = np.dot(v1, v2)
+        sinang = la.norm(np.cross(v1, v2))
+        return np.arctan2(sinang, cosang)
+
+    def get_gem_pose(self):
+        rospy.wait_for_service('/gazebo/get_model_state')
+        try:
+            service_response = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            model_state = service_response(model_name='gem')
+        except rospy.ServiceException as exc:
+            rospy.loginfo("Service call failed: " + str(exc))
+            return 0.0, 0.0, 0.0
+
+        x = model_state.pose.position.x
+        y = model_state.pose.position.y
+
+        orientation_q = model_state.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+
+        return round(x, 4), round(y, 4), round(yaw, 4)
+
+    def start_pp(self):
+        while not rospy.is_shutdown():
+
+            if len(self.path_points_x) < 2:
+                rospy.logwarn("⏳ Waiting for waypoints...")
+                self.rate.sleep()
+                continue
+
+            curr_x, curr_y, curr_yaw = self.get_gem_pose()
+
+            self.path_points_x = np.array(self.path_points_x)
+            self.path_points_y = np.array(self.path_points_y)
+
+            for i in range(len(self.path_points_x)):
+                self.dist_arr[i] = self.dist((self.path_points_x[i], self.path_points_y[i]), (curr_x, curr_y))
+
+            goal_arr = np.where((self.dist_arr < self.look_ahead + 0.3) & (self.dist_arr > self.look_ahead - 0.3))[0]
+
+            found = False
+            for idx in goal_arr:
+                v1 = [self.path_points_x[idx]-curr_x , self.path_points_y[idx]-curr_y]
+                v2 = [np.cos(curr_yaw), np.sin(curr_yaw)]
+                temp_angle = self.find_angle(v1, v2)
+                if abs(temp_angle) < np.pi/2:
+                    self.goal = idx
+                    found = True
+                    break
+
+            if not found:
+                rospy.logwarn("⚠️ No valid goal point ahead.")
+                self.rate.sleep()
+                continue
+
+            L = self.dist_arr[self.goal]
+
+            gvcx = self.path_points_x[self.goal] - curr_x
+            gvcy = self.path_points_y[self.goal] - curr_y
+            goal_x_veh_coord = gvcx*np.cos(curr_yaw) + gvcy*np.sin(curr_yaw)
+            goal_y_veh_coord = gvcy*np.cos(curr_yaw) - gvcx*np.sin(curr_yaw)
+
+            alpha = self.path_points_yaw[self.goal] - curr_yaw
+            k = 0.285
+            angle_i = math.atan((2 * k * self.wheelbase * math.sin(alpha)) / L) 
+            angle = angle_i * 2
+            angle = round(np.clip(angle, -0.61, 0.61), 3)
+
+            ct_error = round(np.sin(alpha) * L, 3)
+
+            rospy.loginfo(f"[PP] Crosstrack Error: {ct_error:.3f} | Steering: {math.degrees(angle):.1f}° | Goal: {self.goal}")
+
+            # self.ackermann_msg.speed = 2.8
+            # self.ackermann_msg.steering_angle = angle
+            # self.ackermann_pub.publish(self.ackermann_msg)
+            self.steer_cmd.angular_position = np.radians(angle) # steering
+            # self.accel_pub.publish(self.accel_cmd)
+            self.steer_pub.publish(self.steer_cmd)
+            # self.turn_pub.publish(self.turn_cmd) # turn signal haha
+
+            self.rate.sleep()
+
+
+def pure_pursuit():
+    rospy.init_node('pure_pursuit_sim_node', anonymous=True)
+    pp = PurePursuit()
+    try:
+        pp.start_pp()
+    except rospy.ROSInterruptException:
+        pass
+
+if __name__ == '__main__':
+    pure_pursuit()
