3f07b8498423fcccf138ed2c8507d22c0050bcc2
Kunal Jain <kunal6@illinois.edu>
2025-04-23 09:35:29 -0500

    Changes to astar_utils

--- e2/src/vehicle_drivers/hybrid_a_star_sim/scripts/astar_utils.py
+++ e2/src/vehicle_drivers/hybrid_a_star_sim/scripts/astar_utils.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 XY_GRID_RESOLUTION = 1.0
 YAW_GRID_RESOLUTION = np.deg2rad(15.0)
+GRID_RES = np.array(XY_GRID_RESOLUTION, XY_GRID_RESOLUTION, YAW_GRID_RESOLUTION)
 MOVE_STEP_SIZE = 1.0
 # WB = 2.0
 WB = 1.75
@@ -29,68 +30,70 @@ def reeds_shepp_path(start, goal, radius):
     return path
 
 class Node:
-    def __init__(self, x_ind, y_ind, yaw_ind, direction, x, y, yaw, cost, parent_index):
-        self.x_ind = x_ind
-        self.y_ind = y_ind
-        self.yaw_ind = yaw_ind
+    def __init__(self, ind, direction, coord, cost, parent_index):
+        self.ind = ind
         self.direction = direction
-        self.x = x
-        self.y = y
-        self.yaw = yaw
+        self.coord = coord
         self.cost = cost
         self.parent_index = parent_index
     def __lt__(self, other):
         return self.cost < other.cost
 
-def calc_index(x, y, yaw):
-    x_ind = round(x / XY_GRID_RESOLUTION)
-    y_ind = round(y / XY_GRID_RESOLUTION)
-    yaw_ind = round(yaw / YAW_GRID_RESOLUTION)
-    return x_ind, y_ind, yaw_ind
+def calc_index(true_coords):
+    return np.round(true_coords / GRID_RES)
+
+def kinematic_bicycle_model(cur_coords, steer, velocity, wheelbase):
+    """
+    Kinematic bicycle model to compute the next state.
+    """
+    next_x = cur_coords[0] + velocity * math.cos(cur_coords[2])
+    next_y = cur_coords[1] + velocity * math.sin(cur_coords[2])
+    next_yaw = cur_coords[2] + (velocity / wheelbase) * math.tan(steer)
+    return np.array([next_x, next_y, next_yaw])
 
 def hybrid_astar(start, goal):
-    start_index = calc_index(start[0], start[1], start[2])
-    goal_index = calc_index(goal[0], goal[1], goal[2])
+    start_coord = np.array(start[0], start[1], start[2])
+    start_index = calc_index(start_coord)
+    goal_index = calc_index(np.array(goal[0], goal[1], goal[2]))
     
     open_list = []
     closed_set = set()
     node_map = {}
 
-    start_node = Node(*start_index, True, start[0], start[1], start[2], 0.0, None)
+    start_node = Node(start_index, True, start_coord, 0.0, None)
     heapq.heappush(open_list, (start_node.cost, start_node))
     node_map[(start_index, True)] = start_node
 
     while open_list:
         _, current = heapq.heappop(open_list)
-        key = ((current.x_ind, current.y_ind, current.yaw_ind), current.direction)
+        key = (tuple(current.ind), current.direction)
 
         if key in closed_set:
             continue
         closed_set.add(key)
 
-        dx = current.x - goal[0]
-        dy = current.y - goal[1]
-        dist_to_goal = math.hypot(dx, dy)
+        # Vectorized distance to goal calculation
+        dist_to_goal = np.hypot(current.x - goal[0], current.y - goal[1])
 
         if dist_to_goal <= 2.0:
             return reconstruct_path(current, node_map, goal)
 
+        # Vectorized loop over steering angles and directions
         for steer in np.linspace(-MAX_STEER, MAX_STEER, N_STEER):
-            for direction in [True]:
-                step = MOVE_STEP_SIZE if direction else -MOVE_STEP_SIZE
-                next_x = current.x + step * math.cos(current.yaw)
-                next_y = current.y + step * math.sin(current.yaw)
-                next_yaw = current.yaw + step / WB * math.tan(steer)
+            for velocity in [MOVE_STEP_SIZE, -MOVE_STEP_SIZE]:
+                next_coord = kinematic_bicycle_model(
+                    current.coord, steer, velocity, WB
+                )
 
-                x_ind, y_ind, yaw_ind = calc_index(next_x, next_y, next_yaw)
-                new_key = ((x_ind, y_ind, yaw_ind), direction)
+                ind = calc_index(next_coord)
+                new_key = (tuple(ind), velocity > 0)
 
                 if new_key in closed_set:
                     continue
 
-                cost = current.cost + MOVE_STEP_SIZE + (0.1 if direction != current.direction else 0.0)
-                new_node = Node(x_ind, y_ind, yaw_ind, direction,
-                                next_x, next_y, next_yaw, cost, key)
+# Calculate cost with direction change penalty
+                cost = current.cost + MOVE_STEP_SIZE + (0.1 if (velocity > 0) != current.direction else 0.0)
+                new_node = Node(ind, velocity > 0, next_coord, cost, key)
 
                 if new_key not in node_map or node_map[new_key].cost > cost:
                     node_map[new_key] = new_node
@@ -102,7 +105,7 @@ def reconstruct_path(goal_node, node_map, goal):
     path = deque()
     node = goal_node
     while node is not None:
-        path.appendleft((node.x, node.y, node.yaw))
+        path.appendleft(node.coord)
         node = node_map.get(node.parent_index)
     path.extend(reeds_shepp_path(path[-1], goal, radius=2.0))
     return list(path)
